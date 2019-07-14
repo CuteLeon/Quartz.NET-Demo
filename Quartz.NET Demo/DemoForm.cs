c3180cd988886455325492d2cbaeb157da85aaea
Leon <Leon.ID@QQ.COM>
2019-07-14 11:35:09 +0800

    显示时间作业；

--- Quartz.NET Demo/DemoForm.cs
+++ Quartz.NET Demo/DemoForm.cs
@@ -7,14 +7,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Quartz.Impl;
+using Quartz.NET_Demo.Services.Jobs;
 
 namespace Quartz.NET_Demo
 {
     public partial class DemoForm : Form
     {
+        /// <summary>
+        /// 调度器
+        /// </summary>
+        IScheduler scheduler;
+
+        /// <summary>
+        /// 调度器工厂
+        /// </summary>
+        ISchedulerFactory factory;
+
         public DemoForm()
         {
-            InitializeComponent();
+            this.InitializeComponent();
+
+            this.factory = new StdSchedulerFactory();
+            this.scheduler = this.factory.GetScheduler().Result;
+            this.scheduler.Start();
+
+            IJobDetail job = JobBuilder.Create()
+                .OfType<DateTimeJob>()
+                .WithIdentity("作业1", "分组1")
+                .WithDescription("我是分组1里的作业1")
+                .Build();
+
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithIdentity("触发器1", "分组1")
+                .WithDescription("我是分组1里的触发器1")
+                .WithCronSchedule("0/5 * * * * ?")
+                .Build();
+
+            this.scheduler.ScheduleJob(job, trigger);
+            this.scheduler.Start();
         }
     }
 }
---
+++ Quartz.NET Demo/Services/Jobs/DateTimeJob.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Quartz.NET_Demo.Services.Jobs
+{
+    public class DateTimeJob : IJob
+    {
+        public DateTimeJob()
+            => Console.WriteLine($"构造作业：{this.GetType().Name} ({this.GetHashCode().ToString("X")})");
+
+        /// <summary>
+        /// 执行作业
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public async Task Execute(IJobExecutionContext context)
+        {
+            await Console.Out.WriteLineAsync($"当前时间：{DateTime.Now.ToLongTimeString()}");
+        }
+    }
+}
